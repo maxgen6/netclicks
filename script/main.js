0d6a9521498d71e71dde681c1028e41520644643
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-05-27 16:03:22 +0300

    второй день

--- script/main.js
+++ script/main.js
@@ -1,8 +1,31 @@
 'use strict';
+
+
+const DBService = class {
+    getData = async (url) => {
+        const res = await fetch(url);
+        if(res.ok){
+            console.log(res.json());
+        }
+    }
+
+    getTestData = () => {
+        return this.getData('test.json');
+    }
+
+}
+
+new DBService().getTestData().then((data) => {
+
+})
+
+
 //меню
 
 const leftMenu = document.querySelector('.left-menu'),
-    hamburger = document.querySelector('.hamburger');
+    hamburger = document.querySelector('.hamburger'),
+    tvShowList = document.querySelector('.tv-shows__list'),
+    modal = document.querySelector('.modal');
 
 //открытие/закрытие меню
 
@@ -36,11 +59,39 @@ const photo = () => {
     const img = document.querySelectorAll('.tv-card__img');
         img.forEach((elem) => {
             elem.addEventListener('mouseenter', (event) => {
+            if(event.target.dataset.backdrop){
             let photoId = event.target.src;
             event.target.src = event.target.dataset.backdrop;
-            
+        
             elem.addEventListener('mouseleave', (event) => event.target.src = photoId); 
+        }
         });
     });
 };
 photo();
+
+//модальное окно
+
+tvShowList.addEventListener('click', (event) => {
+    event.preventDefault();
+    let target = event.target;
+    target = target.closest('.tv-card');
+
+    if(target) {
+        document.body.style.overflow = 'hidden';
+        modal.classList.remove('hide');
+    }
+
+});
+
+//закрытие модального окна
+
+modal.addEventListener('click', (event) => {
+    if(event.target.closest('.cross') ||
+       event.target.classList.contains('modal')) {
+        document.body.style.overflow = '';
+        modal.classList.add('hide');
+    }
+});
+
+
